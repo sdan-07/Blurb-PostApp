c219e943fd74ecb0b65ae5f81d646ee2158da71c
sdan-07 <soumyadipdan7@gmail.com>
2026-03-22 18:16:46 +0530

    fixed scale view

--- frontend/src/components/pages/Login.jsx
+++ frontend/src/components/pages/Login.jsx
@@ -50,7 +50,7 @@ const Login = ({ url }) => {
         <div>
           <form
             onSubmit={HandleSubmit}
-            className="flex border rounded-3xl shadow-amber-400/50 p-8 w-sm md:w-md flex-col gap-4 scale-105 md:scale-110"
+            className="flex border rounded-3xl shadow-amber-400/50 p-8 w-sm md:w-md flex-col gap-4 scale-95 md:scale-110"
           >
             <div>
               <div className="mb-2 block">
