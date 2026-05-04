9edfa1ace4fff7633c931778e805300a101e8e50
sdan-07 <soumyadipdan7@gmail.com>
2026-05-04 23:47:46 +0530

    merged frontend path

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response } from 'express'
 import cookieParser from 'cookie-parser'
+import path from "path";
+import { fileURLToPath } from 'url';
 import cors from 'cors'
 
 //imports
@@ -7,10 +9,14 @@ import postRoutes from './routes/post.route.js'
 import authRoutes from './routes/auth.route.js'
 
 const app = express()
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const publicPath = path.join(__dirname, '..', 'public');
 
 //middlewares
 app.use(cors({
-    origin: [process.env.VITE_FRONTEND_URL as string, "http://127.0.0.1:3001"],
+    origin: process.env.VITE_FRONTEND_URL as string,
+    methods: ['POST', 'GET', 'PATCH', 'DELETE'],
     credentials: true
 }))
 
@@ -18,15 +24,24 @@ app.use(express.json())
 app.use(cookieParser())
 
 //health check route
-app.get('/', (_: Request, res: Response) => {
-    res.status(200).json({
-        status: "OK",
-        message: "Server is running"
-    })
-})
+// app.get('/', (_: Request, res: Response) => {
+//     res.status(200).json({
+//         status: "OK",
+//         message: "Server is running"
+//     })
+// })
 
 //api
 app.use('/api/post', postRoutes)
 app.use('/api/auth', authRoutes)
 
-export default app
+//serve frontend
+app.use(express.static(publicPath))
+
+//spa fallback
+app.get('/{*path}', (_:Request, res: Response) => {
+    res.sendFile(path.join(publicPath, 'index.html'))
+})
+
+
+export default app;
