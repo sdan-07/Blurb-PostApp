bd8963385c4675ead1035ed24879b511817cbf94
sdan-07 <soumyadipdan7@gmail.com>
2026-05-04 23:55:23 +0530

    added health check route

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,47 +1,48 @@
-import express, { Request, Response } from 'express'
-import cookieParser from 'cookie-parser'
+import express, { Request, Response } from "express";
+import cookieParser from "cookie-parser";
 import path from "path";
-import { fileURLToPath } from 'url';
-import cors from 'cors'
+import { fileURLToPath } from "url";
+import cors from "cors";
 
 //imports
-import postRoutes from './routes/post.route.js'
-import authRoutes from './routes/auth.route.js'
+import postRoutes from "./routes/post.route.js";
+import authRoutes from "./routes/auth.route.js";
 
-const app = express()
+const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const publicPath = path.join(__dirname, '..', 'public');
+const publicPath = path.join(__dirname, "..", "public");
 
 //middlewares
-app.use(cors({
+app.use(
+  cors({
     origin: process.env.VITE_FRONTEND_URL as string,
-    methods: ['POST', 'GET', 'PATCH', 'DELETE'],
-    credentials: true
-}))
+    methods: ["POST", "GET", "PATCH", "DELETE"],
+    credentials: true,
+  }),
+);
 
-app.use(express.json())
-app.use(cookieParser())
-
-//health check route
-// app.get('/', (_: Request, res: Response) => {
-//     res.status(200).json({
-//         status: "OK",
-//         message: "Server is running"
-//     })
-// })
+app.use(express.json());
+app.use(cookieParser());
 
 //api
-app.use('/api/post', postRoutes)
-app.use('/api/auth', authRoutes)
+app.use("/api/post", postRoutes);
+app.use("/api/auth", authRoutes);
+
+//health check route
+app.get("/health", (_: Request, res: Response) => {
+  res.status(200).json({
+    status: "OK",
+    message: "Server is running",
+  });
+});
 
 //serve frontend
-app.use(express.static(publicPath))
+app.use(express.static(publicPath));
 
 //spa fallback
-app.get('/{*path}', (_:Request, res: Response) => {
-    res.sendFile(path.join(publicPath, 'index.html'))
-})
-
+app.get("/{*path}", (_: Request, res: Response) => {
+  res.sendFile(path.join(publicPath, "index.html"));
+});
 
 export default app;
